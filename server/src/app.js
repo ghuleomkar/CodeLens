6583fc367d9bd51b7c551241587ac7c42c254b66
Delta Student <ghuleomkar515@gmail.com>
2026-08-02 22:32:19 +0530

    Configure CORS for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,24 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 //middleware
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -17,7 +34,7 @@ app.use("/api/repository", repositoryRoutes);
 //Test Route
 app.get("/",(req,res) => {
     res.json({
-        sucess:true,
+        success:true,
         message: "AI GitHub Code Reviewer Backend is running",
     });
 });
